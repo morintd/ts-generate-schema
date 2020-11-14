0f5e1ad6e401af2478f1b72d438ffeaa0d4c4c26
Teddy MORIN <morin.td@gmail.com>
2020-11-14 17:51:22 +0200

    Correction formatting / build

--- src/example/expected-raw-login-user-response.jsc.ts
+++ src/example/expected-raw-login-user-response.jsc.ts
@@ -1,105 +1,76 @@
 export default {
-  "type": "object",
-  "properties": {
-    "user": {
-      "type": "object",
-      "properties": {
-        "id": {
-          "type": "string"
+  type: 'object',
+  properties: {
+    user: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
         },
-        "createdAt": {
-          "type": "string"
+        createdAt: {
+          type: 'string',
         },
-        "updatedAt": {
-          "type": "string"
+        updatedAt: {
+          type: 'string',
         },
-        "email": {
-          "type": "string"
+        email: {
+          type: 'string',
         },
-        "username": {
-          "type": "string"
+        username: {
+          type: 'string',
         },
-        "firstname": {
-          "type": "string"
+        firstname: {
+          type: 'string',
         },
-        "lastname": {
-          "type": "string"
+        lastname: {
+          type: 'string',
         },
-        "roles": {
-          "type": "array",
-          "items": {
-            "type": "string"
-          }
-        },
-        "signUp": {
-          "type": "object",
-          "properties": {
-            "id": {
-              "type": "string"
-            },
-            "createdAt": {
-              "type": "string"
-            },
-            "updatedAt": {
-              "type": "string"
-            },
-            "validateEmail": {
-              "type": "boolean"
-            },
-            "facebook": {
-              "type": [
-                "null",
-                "string"
-              ]
-            },
-            "gmail": {
-              "type": [
-                "null",
-                "string"
-              ]
-            }
+        roles: {
+          type: 'array',
+          items: {
+            type: 'string',
           },
-          "required": [
-            "createdAt",
-            "facebook",
-            "gmail",
-            "id",
-            "updatedAt",
-            "validateEmail"
-          ]
-        }
-      },
-      "required": [
-        "createdAt",
-        "email",
-        "firstname",
-        "id",
-        "lastname",
-        "roles",
-        "signUp",
-        "updatedAt",
-        "username"
-      ]
-    },
-    "token": {
-      "type": "object",
-      "properties": {
-        "expiresIn": {
-          "type": "number"
         },
-        "accessToken": {
-          "type": "string"
-        }
+        signUp: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+            },
+            createdAt: {
+              type: 'string',
+            },
+            updatedAt: {
+              type: 'string',
+            },
+            validateEmail: {
+              type: 'boolean',
+            },
+            facebook: {
+              type: ['null', 'string'],
+            },
+            gmail: {
+              type: ['null', 'string'],
+            },
+          },
+          required: ['createdAt', 'facebook', 'gmail', 'id', 'updatedAt', 'validateEmail'],
+        },
       },
-      "required": [
-        "accessToken",
-        "expiresIn"
-      ]
-    }
+      required: ['createdAt', 'email', 'firstname', 'id', 'lastname', 'roles', 'signUp', 'updatedAt', 'username'],
+    },
+    token: {
+      type: 'object',
+      properties: {
+        expiresIn: {
+          type: 'number',
+        },
+        accessToken: {
+          type: 'string',
+        },
+      },
+      required: ['accessToken', 'expiresIn'],
+    },
   },
-  "required": [
-    "token",
-    "user"
-  ],
-  "$schema": "http://json-schema.org/draft-07/schema#"
-}
+  required: ['token', 'user'],
+  $schema: 'http://json-schema.org/draft-07/schema#',
+};
