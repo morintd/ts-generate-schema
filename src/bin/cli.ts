183d134efcc66c164426edf41b6b6896451a12e2
Teddy MORIN <morin.td@gmail.com>
2022-11-12 15:19:39 +0100

    fix: configure CLI for --out

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -11,6 +11,9 @@ const args = yargs
   .string('to')
   .default('to', defaultArgs.to)
   .describe('to', 'Extension of generated json-schema')
+  .string('out')
+  .default('out', defaultArgs.out)
+  .describe('out', 'Path to the generated schema directory')
   .string('export')
   .default('export', defaultArgs.export)
   .describe('export', 'How to export generated json-schema from file').argv;
