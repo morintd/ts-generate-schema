db607e9c76f3d22e54063509b07060b7108464ac
Teddy MORIN <morin.td@gmail.com>
2022-11-12 15:40:29 +0100

    chore: typing for v2.1.0

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -3,7 +3,11 @@
 import yargs from 'yargs';
 
 import { run } from '../ts-generate-schema';
-import { defaultArgs } from '../types/Generator';
+import { defaultArgs, TsGenerateSchemaArgs } from '../types/Generator';
+
+type ArgsType = Omit<TsGenerateSchemaArgs, 'pattern'> & {
+  _: string[];
+};
 
 const args = yargs
   .usage('Usage: ts-generate-schema <pattern>')
@@ -16,6 +20,6 @@ const args = yargs
   .describe('out', 'Path to the generated schema directory')
   .string('export')
   .default('export', defaultArgs.export)
-  .describe('export', 'How to export generated json-schema from file').argv;
+  .describe('export', 'How to export generated json-schema from file').argv as ArgsType;
 
 run({ pattern: args._[0], ...args });
